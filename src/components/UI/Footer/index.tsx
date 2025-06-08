b9eb0994f15bce7a7f38f06aad20ae22eb538819
gustallafermrd <36135271+gustallafermrd@users.noreply.github.com>
2025-06-08 20:33:21 -0300

    fixed logo

--- src/components/UI/Footer/index.tsx
+++ src/components/UI/Footer/index.tsx
@@ -3,8 +3,6 @@ import raft_footer_logo from '../../../../public/svgs/raft_footer_logo.svg';
 import qr_code from '../../../../public/svgs/qr_code.svg';
 import ic_google_playstore from '../../../../public/svgs/ic_google_playstore.svg';
 import ic_baseline_apple from '../../../../public/svgs/ic_baseline_apple.svg';
-import ic_chevron_down from '../../../../public/svgs/ic_chevron_down.svg';
-import ic_copyright from '../../../../public/svgs/ic_copyright.svg';
 
 const linksArr = [
   {
@@ -43,23 +41,11 @@ const Footer = () => {
   return (
     <Wrapper>
       <Inner>
-        <FooterLogo>
-          <Image src={raft_footer_logo} alt="raft_footer_logo" />
-        </FooterLogo>
         <FooterMainContent>
           <FooterMiddle>
-            <QRContainer>
-              <QRImageCtn>
-                <Image src={qr_code} alt="qr_code" />
-              </QRImageCtn>
-              <TextCtn>
-                <p>Scan to download App on the Playstore and Appstore.</p>
-                <IconCtn>
-                  <Image src={ic_google_playstore} alt="playstore icon" />
-                  <Image src={ic_baseline_apple} alt="apple icon" />
-                </IconCtn>
-              </TextCtn>
-            </QRContainer>
+            <FooterLogo>
+              <Image src={raft_footer_logo} alt="raft_footer_logo" />
+            </FooterLogo>
             <FooterNavigation>
               {linksArr.map((l, i) => (
                 <GridColumn key={i}>
@@ -74,14 +60,7 @@ const Footer = () => {
             </FooterNavigation>
           </FooterMiddle>
           <FooterBottom>
-            <Translator>
-              <h3>English (United Kingdom)</h3>
-              <Image src={ic_chevron_down} alt="chevron down" />
-            </Translator>
-            <CopyRight>
-              <Image src={ic_copyright} alt="copyright svg" />
-              Raft Corp, LLC.
-            </CopyRight>
+            
           </FooterBottom>
         </FooterMainContent>
       </Inner>
